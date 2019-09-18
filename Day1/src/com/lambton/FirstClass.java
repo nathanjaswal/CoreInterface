15d79eea8711075405e28e33cb3e26449b3db6cf
Nitin Jaswal <nitinj@whizkraft.net>
2019-09-18 19:16:54 -0400

    Initial commit

---
+++ Day1/src/com/lambton/FirstClass.java
@@ -0,0 +1,18 @@
+package com.lambton;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("NItin Jy");
+
+        int a, b, c;
+
+        a = 10;
+        b = 20;
+
+        c = a + b;
+
+        System.out.println("SUM : " + c);
+
+    }
+
+}
