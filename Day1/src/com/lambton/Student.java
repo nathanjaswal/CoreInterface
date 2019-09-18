c1d2e18b45d829b67df1837f3ba6826af7df579d
Nitin Jaswal <nitinj@whizkraft.net>
2019-09-18 19:27:31 -0400

    [Nathan] out put string reduced

---
+++ Day1/src/com/lambton/Student.java
@@ -0,0 +1,14 @@
+package com.lambton;
+
+public class Student {
+    public static void main(String[] args) {
+
+        // comment
+        int studentID;
+        String studentName;
+
+
+
+    }
+
+}
